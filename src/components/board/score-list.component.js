1827c5e4666f7b378e81e68a9321a469917c36cf
Matej <m.djanic@gmail.com>
2020-10-09 18:46:11 +0200

    restyle  user and score tables

--- src/components/board/score-list.component.js
+++ src/components/board/score-list.component.js
@@ -56,28 +56,30 @@ export default class ScoreList extends Component {
     let username = this.props.username;
     let scores = this.state.scores;
     return (
-      <div className="container-custom container-custom-table">
-        <table style={{ width: '100%' }}>
-          <thead>
-            <tr>
-              <th onClick={() => sortTable(0)}>Datum</th>
-              {!username && <th onClick={() => sortTable(1)}>Ime</th>}
-              <th onClick={() => sortTable(2)}>Vrijednost</th>
-            </tr>
-          </thead>
-          <tbody id="tbody">
-            {scores && scores.map(score =>
-              <tr className={"tr"} key={score.id} id={score.id} onClick={() => { this.props.history.push("/scores/" + score.id) }}>
-                <td>{dateFormatMedium.format(DateUtil.getDateFromLocalDateTime(score.date))}</td>
-                {!username && <td>{score.user.username}</td>}
-                <td>{score.value}</td>
-              </tr>)}
-          </tbody>
-        </table>
-        <div className="container-pagination">
-          <div id="pagination" />
+      <div className="container-custom">
+        <div className="container-custom-table">
+          <table className="custom-table">
+            <thead>
+              <tr>
+                <th onClick={() => sortTable(0)}>Datum</th>
+                {!username && <th onClick={() => sortTable(1)}>Ime</th>}
+                <th onClick={() => sortTable(2)}>Vrijednost</th>
+              </tr>
+            </thead>
+            <tbody id="tbody">
+              {scores && scores.map(score =>
+                <tr className={"tr"} key={score.id} id={score.id} onClick={() => { this.props.history.push("/scores/" + score.id) }}>
+                  <td>{dateFormatMedium.format(DateUtil.getDateFromLocalDateTime(score.date))}</td>
+                  {!username && <td>{score.user.username}</td>}
+                  <td>{score.value}</td>
+                </tr>)}
+            </tbody>
+          </table>
+          <div className="container-pagination">
+            <div id="pagination" />
+          </div>
+          <div id="current-page" />
         </div>
-        <div id="current-page" />
       </div>
     );
   }
