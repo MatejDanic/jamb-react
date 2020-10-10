c4266985fa3b23d69f2bf51af4ec68c9521a166b
Matej <m.djanic@gmail.com>
2020-10-10 18:35:56 +0200

    fix info spelling

--- src/components/game/button/rules-button.component.js
+++ src/components/game/button/rules-button.component.js
@@ -11,12 +11,12 @@ export default class RulesButton extends Component {
   }
 
   handleClick() {
-    alert("Bacanjem kockica postižu se odredeni rezultati koji se upisuju u obrazac. Na kraju igre postignuti se rezultati zbrajaju.\n" +
-      "Nakon prvog bacanja, igrac gleda u obrazac i odlucuje hoce li nešto odmah upisati ili ce igrati dalje.\n" +
-      "U jednom potezu igrac može kockice (sve ili samo one koje izabere) bacati tri puta\n" +
-      "Prvi stupac obrasca upisuje se odozgo prema dolje, a drugom obrnuto. U treci stupac rezultati se upisuju bez odredenog redosljeda.\n" +
-      "Cetvrti stupac mora se popunjavati tako da se nakon prvog bacanja najavljuje igra za odredeni rezultat.\n" +
-      "Igrac je obavezan u to polje upisati ostvareni rezultat bez obzira da li mu to nakon tri bacanja odgovara ili ne.\n" +
-      "Rezultat se može, ali ne mora upisati u cetvrti stupac nakon prvog bacanja.");
+    alert("Bacanjem kockica postižu se određeni rezultati koji se upisuju u obrazac. Na kraju igre postignuti se rezultati zbrajaju.\n" +
+      "Nakon prvog bacanja, igrač gleda u obrazac i odlučuje hoće li nešto odmah upisati ili će igrati dalje.\n" +
+      "U jednom potezu igrač može kockice (sve ili samo one koje izabere) bacati tri puta\n" +
+      "Prvi stupac obrasca upisuje se odozgo prema dolje, a drugi obrnuto. U treći stupac rezultati se upisuju bez određenog redoslijeda.\n" +
+      "Četvrti stupac mora se popunjavati tako da se nakon prvog bacanja najavljuje igra za određeni rezultat.\n" +
+      "Igrač je obavezan u to polje upisati ostvareni rezultat bez obzira da li mu to nakon tri bacanja odgovara ili ne.\n" +
+      "Rezultat se može, ali ne mora upisati u četvrti stupac nakon prvog bacanja.");
   }
 }
