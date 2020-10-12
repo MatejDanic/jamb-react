43c1e6d47fcc96f5e20f2f295546f2a3bf641e89
Matej <m.djanic@gmail.com>
2020-10-12 18:48:06 +0200

    update rules with bonus values

--- src/components/game/button/rules-button.component.js
+++ src/components/game/button/rules-button.component.js
@@ -17,6 +17,14 @@ export default class RulesButton extends Component {
       "Prvi stupac obrasca upisuje se odozgo prema dolje, a drugi obrnuto. U treći stupac rezultati se upisuju bez određenog redoslijeda.\n" +
       "Četvrti stupac mora se popunjavati tako da se nakon prvog bacanja najavljuje igra za određeni rezultat.\n" +
       "Igrač je obavezan u to polje upisati ostvareni rezultat bez obzira da li mu to nakon tri bacanja odgovara ili ne.\n" +
-      "Rezultat se može, ali ne mora upisati u četvrti stupac nakon prvog bacanja.");
+      "Rezultat se može, ali ne mora upisati u četvrti stupac nakon prvog bacanja.\n\n" + 
+      "BONUSI:\n" + 
+      "- BROJEVI (Ako je ukupan zbroj u stupcu od 1 do 6 veći ili jednak 60) +30\n" + 
+      "- TRIS +10\n" + 
+      "- SKALA (mala) 35\n" + 
+      "- SKALA (velika) 45\n" + 
+      "- FULL +30\n" + 
+      "- POKER +40\n" + 
+      "- JAMB +50\n");
   }
 }
