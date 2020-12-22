060367c722c4376b46bca03a2f14ea88428bda0a
Matej <m.djanic@gmail.com>
2020-12-22 16:19:46 +0100

    fix popup styling and scoreboard limit

--- src/components/popup/popup.component.js
+++ src/components/popup/popup.component.js
@@ -7,16 +7,17 @@ export default class Popup extends Component {
         let text = this.props.text;
         return (
             <div className="popup">
-                <div className="popup-inner">
-                    <div className="popup-text">
-                        <h2>
-                            <ul className="scoreboard">
+                <div className="popup-window">
+                    <div className="popup-inner">
+                        <div className="popup-text">
+                            <ul >
                                 {text.map(line => <li key={line}>{line}</li>)}
                             </ul>
-                        </h2>
+                        </div>
+
+                        <button className="popup-button" onClick={this.props.onOk}>OK</button>
+                        {this.props.onClose && <button className="popup-button" onClick={this.props.onClose}>Zatvori</button>}
                     </div>
-                    <button className="popup-button" onClick={this.props.onOk}>OK</button>
-                    {this.props.onClose && <button className="popup-button" onClick={this.props.onClose}>Zatvori</button>}
                 </div>
             </div>
         );
