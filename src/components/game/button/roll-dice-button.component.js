83b6728f6011743988563f8c6232cd89513e6353
Matej <m.djanic@gmail.com>
2020-12-22 14:41:00 +0100

    add roll dice image logic and scoreboard popup

--- src/components/game/button/roll-dice-button.component.js
+++ src/components/game/button/roll-dice-button.component.js
@@ -3,29 +3,30 @@ import "./button.css"
 
 export default class RollDiceButton extends Component {
 
-  render() {
-    let rollDisabled = this.props.rollDisabled;
-    let btnClass = this.getBtnClass(this.props.rollCount);
-    return (
-      <button disabled={rollDisabled} className={"button roll-dice bg-light-pink " + btnClass} style={{ backgroundImage: 'url(/images/misc/roll_dice.png)' }} onClick={this.props.onRollDice}></button>
-    )
-  }
+	render() {
+		let rollDisabled = this.props.rollDisabled;
+		let rollCount = this.props.rollCount;
+		let btnClass = this.getBtnClass(rollCount);
+		return (
+			<button disabled={rollDisabled} className={"button roll-dice bg-light-pink " + btnClass} style={{ backgroundImage: "url(/images/misc/" + rollCount  + "_roll.png)" }} onClick={this.props.onRollDice}></button>
+		)
+	}
 
-  getBtnClass(rollCount) {
-    var btnClass;
-    switch (rollCount) {
-      case 1:
-        btnClass = 'one-roll';
-        break;
-      case 2:
-        btnClass = 'two-rolls';
-        break;
-      case 3:
-        btnClass = 'three-rolls';
-        break;
-      default:
-        btnClass = "";
-    }
-    return btnClass;
-  }
+	getBtnClass(rollCount) {
+		var btnClass;
+		switch (rollCount) {
+			case 1:
+				btnClass = 'one-roll';
+				break;
+			case 2:
+				btnClass = 'two-rolls';
+				break;
+			case 3:
+				btnClass = 'three-rolls';
+				break;
+			default:
+				btnClass = "";
+		}
+		return btnClass;
+	}
 }
--- src/components/game/scoreboard/scoreboard.component.js
+++ src/components/game/scoreboard/scoreboard.component.js
@@ -1,59 +1,72 @@
 import React, { Component } from "react";
 import ScoreService from "../../../services/score.service";
+import Popup from "../../popup/popup.component";
 import "./scoreboard.css";
 import "../../../constants/colors.css";
 
 export default class Scoreboard extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      scores: [],
-      scoresToDisplay: []
-    }
-  }
+	constructor() {
+		super();
+		this.state = {
+			scores: [],
+			scoresToDisplay: [],
+			scoreboard: "",
+			showPopup: false
+		}
+		this.togglePopup = this.togglePopup.bind(this);
+	}
 
-  componentDidMount() {
-    ScoreService.getScoreboard().then(
-      response => {
-        let scores = response.data;
-        let scoresToDisplay = [];
-        let i = 1;
-        for (let score in response.data) {
-          scoresToDisplay.push(response.data[score].username + ' ' + response.data[score].value)
-          if (i === 3) break;
-          else i += 1;
-        }
-        this.setState({ scores, scoresToDisplay })
-      },
-      error => {
-        console.log(error.response && error.response.data);
-      }
-    );
-  }
+	componentDidMount() {
+		ScoreService.getScoreboard().then(
+			response => {
+				let scores = response.data;
+				let scoresToDisplay = [];
+				let i = 1;
+				for (let score in response.data) {
+					scoresToDisplay.push(response.data[score].username + ' ' + response.data[score].value)
+					if (i === 3) break;
+					else i += 1;
+				}
+				this.setState({ scores, scoresToDisplay })
+			},
+			error => {
+				console.log(error.response && error.response.data);
+			}
+		);
+	}
 
-  render() {
-    let scores = this.state.scoresToDisplay;
-    return (
-      <button className="scoreboard-button bg-light-pink" onClick={() => this.handleClick()}>
-        <ul className="scoreboard">
-          {scores.length > 0 ? scores.map(score =>
-            <li key={score}>{score}</li>):
-            <div className="scoreboard-empty">Lj<br />e<br />s<br />t<br />v<br />i<br />c<br />a</div>}
-        </ul>
-      </button>
-    )
-  }
+	togglePopup() {
+		this.setState({ showPopup: !this.state.showPopup });
+	}
 
-  handleClick() {
-    let scoreboard = "Najbolji rezultati ovaj tjedan:";
-    let scores = this.state.scores;
-    let i = 1;
-    for (let key in scores) {
-      scoreboard += "\n" + i + ". " + scores[key].username + ' - ' + scores[key].value;
-      if (i === 10) break;
-      else i += 1;
-    }
-    alert(scoreboard)
-  }
+	render() {
+		let scores = this.state.scoresToDisplay;
+		let scoreboard = this.state.scoreboard;
+		return (
+			<div className="scoreboard-button bg-light-pink" onClick={() => this.handleClick()} >
+				<ul className="scoreboard">
+					{scores.length > 0 ? scores.map(score =>
+						<li key={score}>{score}</li>) :
+						<div className="scoreboard-empty">Lj<br />e<br />s<br />t<br />v<br />i<br />c<br />a</div>}
+				</ul>
+				{this.state.showPopup && <Popup text={scoreboard} closePopup={this.togglePopup} />}
+			</div>
+		)
+	}
+
+	handleClick() {
+		let scoreboard = "Najbolji rezultati ovaj tjedan:";
+		let scores = this.state.scores;
+		let i = 1;
+		for (let key in scores) {
+			scoreboard += "\n" + i + ". " + scores[key].username + ' - ' + scores[key].value;
+			if (i === 10) break;
+			else i += 1;
+		}
+		if (scores.length > 0) {
+			this.setState({ scoreboard });
+			this.togglePopup();
+		}
+	}
 }
